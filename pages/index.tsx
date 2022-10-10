0f7b3a46d937ff5cb146d62ca8cdfce82021e551
Jeong-minji <alswl4152@naver.com>
2022-10-10 19:35:34 +0900

    Add: lint-staged

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,7 @@
 import type { NextPage } from 'next'
-import styled from '@emotion/styled'
 
 const Home: NextPage = () => {
-  return (
-    <Wrapper>
-      <Text>Hello!</Text>
-    </Wrapper>
-  )
+  return <h1>main</h1>
 }
-
-const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.color.background_color};
-  padding: ${({ theme }) => theme.figure * 3}px;
-  height: 100vh;
-`
-
-const Text = styled.h1`
-  color: ${({ theme }) => theme.color.gray_900};
-`
 
 export default Home
